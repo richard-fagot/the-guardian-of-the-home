39e3b464e9119672d320e175b94ba2f9ca29c0d1
Richard Fagot <richard.fagot@gmail.com>
2019-10-15 17:32:17 +0200

    Ajout du test de déserialisation d'un retour de l'API DetectObject

--- src/test/kotlin/home/guardian/azure/ObjectDetectedResponseTest.kt
+++ src/test/kotlin/home/guardian/azure/ObjectDetectedResponseTest.kt
@@ -33,4 +33,13 @@ class ObjectDetectedResponseTest {
 
          Assertions.assertEquals("{\"objects\":[{\"rectangle\":{\"y\":2,\"x\":1,\"w\":3,\"h\":4},\"object\":\"Type\",\"confidence\":0.8},{\"rectangle\":{\"y\":6,\"x\":5,\"w\":7,\"h\":8},\"object\":\"Type2\",\"confidence\":0.1}]}", result)
     }
+
+    @Test
+    fun `Une chaine json de retour de l'API DetectObject est correctement désérialisée` () {
+        val objectDetectResponse = "{\"objects\": [{\"rectangle\": {\"x\": 0,\"y\": 0,\"w\": 50,\"h\": 50},\"object\": \"tree\",\"confidence\": 0.9,\"parent\": {\"object\": \"plant\",\"confidence\": 0.95}}],\"requestId\": \"1ad0e45e-b7b4-4be3-8042-53be96103337\",\"metadata\": {\"width\": 100,\"height\": 100,\"format\": \" Jpeg\"}}"
+
+        val jsonb = JsonbBuilder.create()
+ 
+        var objDetected = jsonb.fromJson(objectDetectResponse, ObjectDetectedResponse::class.java)
+    }
 }
